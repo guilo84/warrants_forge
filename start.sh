2c1b818170274344b9ab9889a3e22dfbdf9fdba1
guilo84 <mathew.tolbert@gmail.com>
2026-05-14 20:58:04 -0400

    Initial commit: PaperMC server baseline and startup script

---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+    # Aikar's Flags are the gold standard for Minecraft garbage collection optimization.
+    # Adjust the -Xms and -Xmx values based on the RAM you want to allocate.
+    java -Xms4G -Xmx4G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar paper.jar nogui
+
